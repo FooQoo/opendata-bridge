80a02dca35a24ed33e6dd14ef53ddae6abafee8f
FooQoo <horseshoes.buzz.17@gmail.com>
2022-08-12 23:38:01 +0900

    fix lintstagedrc

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -5,6 +5,18 @@ const buildEslintCommand = (filenames) =>
     .map((f) => path.relative(process.cwd(), f))
     .join(' --file ')}`
 
+const buildPrettierCommand = (filenames) =>
+  `yarn format --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`
+
+const buildStylelintCommand = (filenames) =>
+  `yarn stylelint:fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`
+
 module.exports = {
   '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*': [buildPrettierCommand],
+  '*.{css,scss}': [buildStylelintCommand]
 }
