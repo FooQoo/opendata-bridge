3e108a6d94b692b5f96a0ed3eb6bf76c8928ba2f
FooQoo <horseshoes.buzz.17@gmail.com>
2022-08-13 10:13:05 +0900

    add article api

---
+++ src/pages/api/article.ts
@@ -0,0 +1,39 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Article = {
+  link: string;
+  title: string;
+  description: string;
+};
+
+const stub = [
+  {
+    link: 'https://nextjs.org/docs',
+    title: 'Documentation',
+    description: 'Find in-depth information about Next.js features and API.',
+  },
+  {
+    link: 'https://nextjs.org/learn',
+    title: 'Learn',
+    description: 'Learn about Next.js in an interactive course with quizzes!',
+  },
+  {
+    link: 'https://github.com/vercel/next.js/tree/canary/examples',
+    title: 'Examples',
+    description: 'Discover and deploy boilerplate example Next.js projects.',
+  },
+  {
+    link: 'https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
+    title: 'Deploy',
+    description:
+      'Instantly deploy your Next.js site to a public URL with Vercel.',
+  },
+];
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Article[]>
+) {
+  res.status(200).json(stub);
+}
