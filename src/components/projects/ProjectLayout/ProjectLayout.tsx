ff6cda12a3e79e3d0f11538aaf48594c9c0cd373
FooQoo <horseshoes.buzz.17@gmail.com>
2023-08-27 14:28:39 +0900

    フッターを削除

--- src/components/projects/ProjectLayout/ProjectLayout.tsx
+++ src/components/projects/ProjectLayout/ProjectLayout.tsx
@@ -3,7 +3,6 @@
 import 'app/globals.scss';
 
 import { Providers } from 'app/provider';
-import { Footer } from 'components/organisms/Footer/Footer';
 import { Header } from 'components/organisms/Header/Header';
 import { FC, ReactNode } from 'react';
 import { RecoilRoot } from 'recoil';
@@ -19,7 +18,6 @@ const ProjectLayout: FC<{ children: ReactNode }> = ({ children }) => {
           <main className={styles.container}>
             <Header />
             <div className={styles.main}>{children}</div>
-            <Footer />
           </main>
         </Providers>
       </RecoilRoot>
