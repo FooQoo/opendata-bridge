8fc5fe479f70c938e596e84dbda8b75d2b344ad9
FooQoo <horseshoes.buzz.17@gmail.com>
2023-09-04 08:34:09 +0900

    GPT3.5に変更

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -82,7 +82,7 @@ export async function POST(req: Request) {
         return openai.createChatCompletion({
           messages: [...messages, ...newMessages],
           stream: true,
-          model: 'gpt-4',
+          model: 'gpt-3.5-turbo',
           functions,
           temperature: 0.0,
         });
