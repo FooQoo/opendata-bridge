b35ead47fcad7ede936a970a97665eb81c3ff414
FooQoo <horseshoes.buzz.17@gmail.com>
2023-09-03 22:17:26 +0900

    2カラムに変更

--- src/app/PromptTemplateList.tsx
+++ src/app/PromptTemplateList.tsx
@@ -42,46 +42,22 @@ const PromptTemplateList = ({ initial }: { initial: UsecaseProps[] }) => {
       {!query.isTyping && (!isLoading || query.isInitialRender) ? (
         usecaseResult
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-0  md:gap-4 mx-auto md:mx-[100px] justify-items-center">
-          <Card className={styles.card} radius="sm">
-            <div className="space-y-3">
-              <Skeleton className="w-3/5 rounded-lg">
-                <div className="h-3 w-3/5 rounded-lg bg-default-200"></div>
-              </Skeleton>
-              <Skeleton className="w-4/5 rounded-lg">
-                <div className="h-3 w-4/5 rounded-lg bg-default-200"></div>
-              </Skeleton>
-              <Skeleton className="w-2/5 rounded-lg">
-                <div className="h-3 w-2/5 rounded-lg bg-default-300"></div>
-              </Skeleton>
-            </div>
-          </Card>
-          <Card className={styles.card} radius="sm">
-            <div className="space-y-3">
-              <Skeleton className="w-3/5 rounded-lg">
-                <div className="h-3 w-3/5 rounded-lg bg-default-200"></div>
-              </Skeleton>
-              <Skeleton className="w-4/5 rounded-lg">
-                <div className="h-3 w-4/5 rounded-lg bg-default-200"></div>
-              </Skeleton>
-              <Skeleton className="w-2/5 rounded-lg">
-                <div className="h-3 w-2/5 rounded-lg bg-default-300"></div>
-              </Skeleton>
-            </div>
-          </Card>
-          <Card className={styles.card} radius="sm">
-            <div className="space-y-3">
-              <Skeleton className="w-3/5 rounded-lg">
-                <div className="h-3 w-3/5 rounded-lg bg-default-200"></div>
-              </Skeleton>
-              <Skeleton className="w-4/5 rounded-lg">
-                <div className="h-3 w-4/5 rounded-lg bg-default-200"></div>
-              </Skeleton>
-              <Skeleton className="w-2/5 rounded-lg">
-                <div className="h-3 w-2/5 rounded-lg bg-default-300"></div>
-              </Skeleton>
-            </div>
-          </Card>
+        <div className="grid grid-cols-1 2xl:grid-cols-2 gap-0  md:gap-4 mx-auto md:mx-[100px] justify-items-center">
+          {[...Array(4)].map((_, index) => (
+            <Card key={index} className={styles.card} radius="sm">
+              <div className="space-y-3">
+                <Skeleton className="w-3/5 rounded-lg">
+                  <div className="h-3 w-3/5 rounded-lg bg-default-200"></div>
+                </Skeleton>
+                <Skeleton className="w-4/5 rounded-lg">
+                  <div className="h-3 w-4/5 rounded-lg bg-default-200"></div>
+                </Skeleton>
+                <Skeleton className="w-2/5 rounded-lg">
+                  <div className="h-3 w-2/5 rounded-lg bg-default-300"></div>
+                </Skeleton>
+              </div>
+            </Card>
+          ))}
         </div>
       )}
     </>
