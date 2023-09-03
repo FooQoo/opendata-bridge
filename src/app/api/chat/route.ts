d7f19d0c5c00a812bf20e074989c4baadc1dd65d
FooQoo <horseshoes.buzz.17@gmail.com>
2023-09-03 17:54:05 +0900

    gpt3.5に戻す

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -69,7 +69,7 @@ export async function POST(req: Request) {
   // const recentMessages = messages.slice(-4);
 
   const response = await openai.createChatCompletion({
-    model: 'gpt-4-32k',
+    model: 'gpt-3.5-turbo-16k',
     stream: true,
     messages,
     functions,
@@ -97,7 +97,7 @@ export async function POST(req: Request) {
         return openai.createChatCompletion({
           messages: [...messages, ...newMessages],
           stream: true,
-          model: 'gpt-4-32k',
+          model: 'gpt-3.5-turbo-16k',
           functions,
           temperature: 0.0,
         });
@@ -109,7 +109,7 @@ export async function POST(req: Request) {
         return openai.createChatCompletion({
           messages: [...messages, ...newMessages],
           stream: true,
-          model: 'gpt-4-32k',
+          model: 'gpt-3.5-turbo-16k',
           functions,
           temperature: 0.0,
         });
